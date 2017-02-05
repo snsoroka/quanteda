f01155322581999197d5e87c586f16324d79fd03
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-02-05 11:51:15 +0000

    Update benchmark

--- tests/benchmarks/benchmark_tokens/recompile.R
+++ tests/benchmarks/benchmark_tokens/recompile.R
@@ -3,18 +3,18 @@ load("/home/kohei/Documents/Brexit/Analysis/data_corpus_guardian.RData")
 toks <- tokens(data_corpus_guardian)
 
 attr_org <- attributes(toks)
-toks <- lapply(unclass(toks), function(x) x[(x %% 2) == 0])
-toks <- as.tokens(toks)
-attributes(toks) <- attr_org
+toks2 <- lapply(unclass(toks), function(x) x[(x %% 2) == 0])
+toks2 <- as.tokens(toks2)
+attributes(toks2) <- attr_org
 
 microbenchmark::microbenchmark(
-    r=quanteda:::tokens_hashed_recompile(toks),
-    cpp=quanteda:::qatd_cpp_recompile(toks, attr(toks, "types")),
-    times = 1
+    r=quanteda:::tokens_hashed_recompile(toks2),
+    cpp=quanteda:::qatd_cpp_recompile(toks2, attr(toks2, "types")),
+    times = 5
 )
 
-r <- quanteda:::tokens_hashed_recompile(toks)
-cpp <- quanteda:::qatd_cpp_recompile(toks, attr(toks, "types"))
+r <- quanteda:::tokens_hashed_recompile(toks2)
+cpp <- quanteda:::qatd_cpp_recompile(toks, attr(toks2, "types"))
 class(cpp) <- 'tokens'
 
-any(unlist(as.list(r)) != unlist(as.list(cpp)))
+#any(unlist(as.list(r)) != unlist(as.list(cpp)))
