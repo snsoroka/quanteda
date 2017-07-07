4c7806a442818d5b5f125231286ab5f0ab93caa0
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-07-07 11:35:38 +0100

    Add benchmark

--- tests/benchmarks/benchmark_tokens/select.R
+++ tests/benchmarks/benchmark_tokens/select.R
@@ -1,9 +1,8 @@
 library(quanteda)
 
-load("/home/kohei/Documents/Brexit/Analysis/data_corpus_guardian.RData")
-docvars(toks, 'year') <- format(docvars(data_corpus_guardian, 'date'), '%Y')
+load("/home/kohei/Documents/Brexit/Analysis/data_tokens_guardian.RData")
+toks <- data_tokens_guardian
 
-toks <- tokens(data_corpus_guardian, verbose=TRUE)
 #toks <- tokens(corpus_subset(data_corpus_guardian, year == 2012))
 toks[[1]]
 
@@ -25,3 +24,27 @@ microbenchmark::microbenchmark(
 
 profvis::profvis(tokens_select(toks, stopwords(), valuetype='fixed'))
 profvis::profvis(tokens_select(toks, stopwords(), valuetype='glob'))
+
+# XPointer -----------------------------------------------
+
+toks <- data_tokens_guardian
+xpt <- as.xtokens(toks)
+
+dict <- 1:100
+toks2 <- quanteda:::qatd_cpp_tokens_select(toks, attr(toks, 'types'), dict, 1, TRUE)
+xpt2 <- quanteda:::qatd_cpp_xpointer_select(xpt, attr(toks, 'types'), dict, 1, TRUE)
+toks3 <- quanteda:::qatd_cpp_tokens(xpt2, attr(toks, 'types'))
+identical(toks2, toks3)
+
+microbenchmark::microbenchmark(
+    quanteda:::qatd_cpp_xpointer_select(xpt, attr(toks, 'types'), dict, 1, TRUE),
+    times = 10
+)
+
+microbenchmark::microbenchmark(
+    quanteda:::qatd_cpp_tokens_select(toks, attr(toks, 'types'), dict, 1, TRUE),
+    quanteda:::qatd_cpp_xpointer_select(xpt, attr(toks, 'types'), dict, 1, TRUE),
+    times = 10
+)
+
+
