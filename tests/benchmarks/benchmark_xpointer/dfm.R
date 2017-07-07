f6c2d5e9527dd0b1b57e7fd9366231b6c88c49e8
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-07-07 11:34:12 +0100

    Add benchmark

--- tests/benchmarks/benchmark_xpointer/dfm.R
+++ tests/benchmarks/benchmark_xpointer/dfm.R
@@ -3,9 +3,7 @@ require(quanteda)
 compile_dfm.tokens2 <- function(x) {
     
     attrs <- attributes(x) 
-    attributes(x) <- NULL
-    index <- qatd_cpp_xpointer_unlist(x)
-    attributes(x) <- attrs
+    index <- quanteda:::qatd_cpp_xpointer_unlist(x)
     
     # shift index for padding, if any
     if (attrs$padding) {
@@ -16,36 +14,35 @@ compile_dfm.tokens2 <- function(x) {
     temp <- Matrix::sparseMatrix(j = index, 
                          p = attr(index, 'position'), 
                          x = 1L, 
-                         dims = c(length(attrs$docnames), length(attrs$types)),
-                         dimnames = list(docs = attrs$docnames,
+                         dims = c(length(docvars(x, '_docname')), length(attrs$types)),
+                         dimnames = list(docs = docvars(x, '_docname'),
                                          features = as.character(attrs$ttypes)))
     new("dfmSparse", temp)
 }
 
 toks <- tokens(data_corpus_irishbudget2010)
 
-load("/home/kohei/Documents/Brexit/Analysis/data_tokens_guardian.RData")
-toks <- data_tokens_guardian
+#load("/home/kohei/Documents/Brexit/Analysis/data_tokens_guardian.RData")
+#toks <- data_tokens_guardian
 
-xpt <- qatd_cpp_xpointer(toks)
-
-attr(xpt, 'types') <- attr(toks, 'types')
-attr(xpt, 'padding') <- FALSE
-attr(xpt, 'docnames') <- docnames(toks)
+xpt <- as.xtokens(toks)
+head(xpt)
+tail(xpt)
+docnames(xpt)
 
 microbenchmark::microbenchmark(
-    quanteda:::compile_dfm.tokens(toks),
+    quanteda:::compile_dfm.tokens(toks, verbose = FALSE),
     compile_dfm.tokens2(xpt),
-    times = 5
+    times = 100
 )
 
 microbenchmark::microbenchmark(
-    qatd_cpp_xpointer_unlist(xpt),
+    quanteda:::qatd_cpp_xpointer_unlist(xpt),
     unlist(unclass(toks), use.names = FALSE),
     times = 100
 )
 
-out1 <- qatd_cpp_xpointer_unlist(xpt)
+out1 <- quanteda:::qatd_cpp_xpointer_unlist(xpt)
 out2 <- unlist(unclass(toks), use.names = FALSE)
 
 tail(out1)
